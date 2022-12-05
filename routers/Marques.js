2ee825d78aa6e9b123e77f6f39c475c06c2bae50
Lucas <lucaslagnes@orange.fr>
2022-12-05 18:57:40 +0100

    Route Marques terminé

--- routers/Marques.js
+++ routers/Marques.js
@@ -59,4 +59,34 @@ router.get('*', function (request, response) {
     response.status(404).send();
 });
 
+router.patch('/:id', function (request, response) {
+    let { id } = request.params;
+
+    Cars.update(
+        {
+          name: request.body.name
+        },
+        {
+          where: {id}
+        }
+      ).then((cars) => {
+        Cars.findByPk(id).then((cars) => {
+                response.json(cars);
+            });
+    }, (validation) => {
+        response.status(400).json({
+            errors: validation.errors.map((error) => {
+                return {
+                    attribute: error.path,
+                    message: error.message
+                };
+            })
+        });
+    });
+});
+
+router.patch('*', function (request, response) {
+    response.status(404).send();
+});
+
 module.exports = router;
