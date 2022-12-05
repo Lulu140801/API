8d96442197e33df35545cb9fe17aaeaedc00cc16
Lucas <lucaslagnes@orange.fr>
2022-12-05 14:51:06 +0100

    ....

--- models/modeles.js
+++ models/modeles.js
@@ -1,23 +1,25 @@
 const Sequelize = require('sequelize');
-var sequelize = new Sequelize('sqlite:db.sqlite');
+const db = require('./index');
 
-module.exports = sequelize.define('playlist', {
-    id: {
-        field: 'PlaylistId',
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    name: {
-        field: 'Name',
-        type: Sequelize.STRING,
-        validate: {
-            notEmpty: {
-                args: true,
-                msg: 'name cannot be empty'
+module.exports = (instance) => {
+    return instance.define('modeles', {
+        id: {
+            field: 'modelesId',
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            field: 'name',
+            type: Sequelize.STRING,
+            validate: {
+                notEmpty: {
+                    args: true,
+                    msg: 'name cannot be empty'
+                }
             }
         }
-    }
-}, {
-    timestamps: false
-});
+    }, {
+        timestamps: false
+    });
+}
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const db = require('./models/index');
 const app = require('./app');
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000 !');
-});
+db.instance.sync({force: true}).then(() => {
+    console.log('Database connected an synchronized');
+
+    app.listen(3000, () => {
+        console.log('Server running on port 3000 !');
+    });
+})
