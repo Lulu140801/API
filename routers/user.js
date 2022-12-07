194c0534487cdd2566ff21cb8db34d5382d60afd
Lucas <lucaslagnes@orange.fr>
2022-12-07 18:01:08 +0100

    merge

--- routers/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const userCtrl = require('../controler/user');
-
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
-
-module.exports = router;
